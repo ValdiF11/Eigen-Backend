d8c9601085fa271d78d680316e593e8046e9ba44
ValdiF11 <valdifirstianto@gmail.com>
2024-06-04 12:18:51 +0700

    feat: migration DB

---
+++ Backend/models/member.js
@@ -0,0 +1,70 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class Member extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      Member.hasMany(models.BorrowedBook, {
+        foreignKey: "MemberId",
+      });
+    }
+  }
+  Member.init(
+    {
+      code: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notEmpty: {
+            msg: "code is required",
+          },
+          notNull: {
+            msg: "code is required",
+          },
+        },
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notEmpty: {
+            msg: "name is required",
+          },
+          notNull: {
+            msg: "name is required",
+          },
+        },
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notEmpty: {
+            msg: "Password is required",
+          },
+          notNull: {
+            msg: "Password is required",
+          },
+          len: {
+            args: 5,
+            msg: "Password atleast 5 character",
+          },
+        },
+      },
+      penaltyStatus: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: "false",
+      },
+      penaltyDate: DataTypes.DATE,
+    },
+    {
+      sequelize,
+      modelName: "Member",
+    }
+  );
+  return Member;
+};
